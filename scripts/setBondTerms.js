aa15bf6fc5b931dde0fadbe314ea383ca032a1e8
bitdeep <19378023+bitdeep@users.noreply.github.com>
2021-12-10 19:06:57 -0300

    setBondTerms real contract

--- scripts/setBondTerms.js
+++ scripts/setBondTerms.js
@@ -7,8 +7,8 @@ const CONTRACT = '0x28b18E5ca5b8C1E0D4c3A25BC0b5D3BE4e126c2C';
 const _OlympusBondDepository = artifacts.require("OlympusBondDepository");
 let bond;
 module.exports = async function (deployer, network, accounts) {
-  bond = await _OlympusBondDepository.deployed();
-  // bond = await _OlympusBondDepository.at(CONTRACT);
+  // bond = await _OlympusBondDepository.deployed();
+  bond = await _OlympusBondDepository.at(CONTRACT);
   await dumpInfo('BEFORE');
 
   await bond.setBondTerms ( 0, (HOURS/2)*3600 );
