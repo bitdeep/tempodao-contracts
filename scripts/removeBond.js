0b59c820c9cc16c61d8c911a67c18d1098198bf2
bitdeep <19378023+bitdeep@users.noreply.github.com>
2021-12-10 18:08:48 -0300

    now with toggle status

--- scripts/removeBond.js
+++ scripts/removeBond.js
@@ -1,7 +1,7 @@
 // toggle a bond on/off, after running this script go to allBonds.ts
 // to add remove the bond contract.
-// const bondContract = '0x28b18E5ca5b8C1E0D4c3A25BC0b5D3BE4e126c2C';
-const bondContract = '0xa3c11b4c3ce521AbD444D7F9308c13912eCdEAcC';
+const bondContract = '0x28b18E5ca5b8C1E0D4c3A25BC0b5D3BE4e126c2C';
+// const bondContract = '0xa3c11b4c3ce521AbD444D7F9308c13912eCdEAcC';
 
 
 
@@ -18,8 +18,8 @@ const bondContract = '0xa3c11b4c3ce521AbD444D7F9308c13912eCdEAcC';
 
 const OlympusTreasury = artifacts.require("OlympusTreasury");
 module.exports = async function (deployer, network, accounts) {
-  const treasure = await OlympusTreasury.deployed();
-  // const treasure = await OlympusTreasury.at('0x2af791E7EBa7efF93485CF8516bAf7bdc94d4db7');
+  // const treasure = await OlympusTreasury.deployed();
+  const treasure = await OlympusTreasury.at('0x2af791E7EBa7efF93485CF8516bAf7bdc94d4db7');
   let isReserveDepositor = (await treasure.isReserveDepositor(bondContract));
   let isLiquidityDepositor = (await treasure.isLiquidityDepositor(bondContract));
 
